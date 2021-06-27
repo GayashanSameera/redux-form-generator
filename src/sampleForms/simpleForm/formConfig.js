824a22f755a3b9ac5cdc19696d216e9028844b66
GayashanSameera <gayashansameera30@gmail.com>
2021-06-27 17:45:03 +0530

    validations

--- src/sampleForms/simpleForm/formConfig.js
+++ src/sampleForms/simpleForm/formConfig.js
@@ -1,16 +1,22 @@
+import { required } from "redux-form-validators";
+
 import { INPUT_FIELD, FORM_TEMPLATES } from "../../formTemplates/constants";
 const { FULL_CONTAINER } = FORM_TEMPLATES;
 
-export const formFields = (dirtyFormValues = {}, schemeId) => [
-    {
-        type: FULL_CONTAINER,
-        bool: true,
-        label: "Activation code",
-        field: {
-            __order: "a",
-            name: "activationCode",
-            className: "form-control",
-            component: INPUT_FIELD,
+export const formFields = (dirtyFormValues = {}, schemeId) => {
+    console.log('dirtyFormValues...',dirtyFormValues);
+    return [
+        {
+            type: FULL_CONTAINER,
+            bool: true,
+            label: "Activation code",
+            field: {
+                __order: "a",
+                name: "activationCode",
+                className: "form-control",
+                component: INPUT_FIELD,
+                validate: [required({ message: "Required" })],
+            },
         },
-    },
-];
+    ];
+};
