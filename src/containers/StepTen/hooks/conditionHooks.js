5e943469a3a8f7c33070fe4ad377855edb74f8ec
GayashanSameera <gayashansameera30@gmail.com>
2021-06-29 19:22:11 +0530

    new conditions

--- src/containers/StepTen/hooks/conditionHooks.js
+++ src/containers/StepTen/hooks/conditionHooks.js
@@ -1,7 +1,8 @@
 export const conditionHooks = (data) => {
 
     return {
-        "activeWorkFlowCondition": data.a === a && data.c === "K",
+        "activationCodeTypeEnable": data.activationCode !== '35131' && data.step === 9,
+        "activationNameEnable": data.activationCode !== '35131' && data.activationCodeType === "K",
     }
 
 }
