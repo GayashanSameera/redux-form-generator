4d9276cf3a84d653442ea2a3d4c46a6421a753f7
GayashanSameera <gayashansameera30@gmail.com>
2021-07-01 13:45:35 +0530

    item prediction

--- src/facedetection/new.js
+++ src/facedetection/new.js
@@ -2,6 +2,8 @@ import React, { useRef } from "react";
 import Webcam from "react-webcam";
 import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+
 import { drawMesh } from "./util";
 
 const FaceDetection = (props) => {
@@ -9,22 +11,30 @@ const FaceDetection = (props) => {
     const webcamRef = useRef(null);
     const canvasRef = useRef(null);
 
+    let cocossdModel = null;
     //load facemesh
     const runFacemesh = async (data) => {
         const net = await facemesh.load({
             inputResolution: { width: 640, height: 580 },
             scale: 0.8,
         });
-        setInterval(()=>{detect(net)},100);
+
+        setInterval(() => {
+            detect(net);
+        }, 1000);
     };
 
     //detect function
     const detect = async (net) => {
-        if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readyState === 4) {
+        if (
+            typeof webcamRef.current !== "undefined" &&
+            webcamRef.current !== null &&
+            webcamRef.current.video.readyState === 4
+        ) {
             //get video properties
             const video = webcamRef.current.video;
-            const videoWidth =  webcamRef.current.video.videoWidth;
-            const videoHeight =  webcamRef.current.video.videoHeight;
+            const videoWidth = webcamRef.current.video.videoWidth;
+            const videoHeight = webcamRef.current.video.videoHeight;
 
             //set video width
             webcamRef.current.video.width = videoWidth;
@@ -36,13 +46,24 @@ const FaceDetection = (props) => {
 
             //make detections
             const face = await net.estimateFaces(video);
-            console.log('face......',face);
+            console.log("face......", face);
             //get canvas context for drawing
-            const ctx = canvasRef.current.getContext('2d');
-            drawMesh(face,ctx);
+            const ctx = canvasRef.current.getContext("2d");
+            drawMesh(face, ctx);
+
+            // Load the model.
+            cocoSsd.load().then((model) => {
+                // detect objects in the image.
+                model.detect(video).then((predictions) => {
+                    console.log("Predictions: ", predictions);
+                    ctx.font = "30px Arial";
+                    ctx.fillText("Hello World", 10, 50);
+                });
+            });
+            
         }
     };
-    runFacemesh()
+    runFacemesh();
     return (
         <div>
             <Webcam
